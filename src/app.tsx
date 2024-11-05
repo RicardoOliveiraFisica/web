c0fdba4efefc835a040d41d940127f1cd1e79ee9
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2024-11-05 00:03:41 -0300

    create summary

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,15 @@
-import { Plus } from 'lucide-react'
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
-import { Button } from './components/ui/button'
+import { Dialog, DialogTrigger } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { EmptyGoals } from './components/empty-goals'
+import { Summary } from './components/summary'
 // JSX - Javascript XML -> HTML
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="in.orbit" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-      {/* <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button> */}
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar meta
-      </Button>
-    </div>
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      <CreateGoal />
+    </Dialog>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import './index.css'
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
